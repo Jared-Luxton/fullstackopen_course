a311c62498a2b70347a029a4d4f2df78c68cc8da
JJL <jaredjamesluxton@gmail.com>
2023-01-28 14:14:10 -0700

    Main

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-// ooo k
+// ooo k k k
 ReactDOM.createRoot(document.getElementById('root')).root.render(<App />)
